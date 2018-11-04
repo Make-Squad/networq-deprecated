acf160879828e88ed682664ecbccc1246cc4eeb1
Johnathan Chen <johnathanchenn@outlook.com>
2018-11-03 17:27:16 -0700

    auth place changes

--- auth-demo/src/index.js
+++ auth-demo/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import TodoModel from './TodoModel'
 import 'todomvc-common/base.css';
 import 'todomvc-app-css/index.css';
 
-var model = new TodoModel('react-todos');
 
 function render() {
-  console.log("render callback")
+
   ReactDOM.render(
-    <App model={model}/>,
+    <App/>,
     document.getElementsByClassName('todoapp')[0]
   );
 }
-window._model = model;
-model.subscribe(render);
 render();
